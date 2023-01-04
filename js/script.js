0ccb81e6c8522b09f65ff3ce35866f7616da6489
jijijung <jjihyun054@gmail.com>
2023-01-04 17:02:13 +0900

    content1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const toggleBtn = document.querySelector('.h-toggleBtn');
 const menu = document.querySelector('.h-menu');
 const btn = document.querySelector('.h-btn');
+const main = document.querySelector('#main');
 
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
   btn.classList.toggle('active');
+  main.classList.toggle('add');
 });
